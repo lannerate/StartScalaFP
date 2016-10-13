09456bb4ca7b53c4b32df2df4baa4a25f99b1bd7
hzhang3 <zhanghuizaizheli@hotmail.com>
2016-10-13 16:16:06 +0800

    implemnt the function map() for Option, and define get value and isEmpty value

--- src/main/scala/fp/datastructs/Option.scala
+++ src/main/scala/fp/datastructs/Option.scala
@@ -4,17 +4,32 @@ package fp.datastructs
   * Created by hzhang3 on 10/13/2016.
   */
 
-sealed trait Option[+A]
+sealed trait Option[+A] {
+  def isEmpty:Boolean
+  def get:A
+  def map[B](f:A=>B):Option[B]
+}
 
-case class Some[+A](get: A) extends Option[A]
+case class Some[+A](x: A) extends Option[A] {
+  override def isEmpty:Boolean = false
 
-case object None extends Option[Nothing]
+  override def get = x
+
+  override def map[B](f: A => B): Option[B] = Some(f(x))
+}
+
+case object None extends Option[Nothing]{
+  override def isEmpty:Boolean = true
+
+  override def get = throw new NoSuchElementException("None have not value!")
+
+  override def map[B](f: Nothing => B): Option[B] = None
+}
 
 object Option {
 
   def mean(xs:Seq[Double]):Option[Double] =
     if(xs.isEmpty) None
     else Some(xs.sum / xs.length)
-
 
 }
--- src/test/scala/fp/datastructs/OptionTest.scala
+++ src/test/scala/fp/datastructs/OptionTest.scala
@@ -10,8 +10,14 @@ class OptionTest extends FunSpec{
   describe("test Option function"){
 
     it("test the function mean() using Option[A]"){
-      val mean = Option.mean(Seq(100,273,35,199,200))
-      assert(mean == Some(161.4))
+      val mean = Option.mean(Seq(100,273,35,199,200)).get
+      assert(mean == 161.4)
     }
+
+    it("test the function map() of Option"){
+      val maped = Some(123).map(_ * 2 ).get
+      assert(maped == 246)
+    }
+
   }
 }
