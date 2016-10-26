2cc3f37e00abf578c04b07ced76b2c08b733de0d
hui <zhanghuizaizhe@hotmail.com>
2016-10-26 23:26:17 +0800

    implement the function orElse()

--- src/main/scala/fp/datastructs/Either.scala
+++ src/main/scala/fp/datastructs/Either.scala
@@ -11,7 +11,10 @@ sealed trait Either[+E, +A] {
     case Left(e) => Left(e)
     case Right(a) => f(a)
   }
-
+  def orElse[EE >: E, AA >: A](b: => Either[EE, AA]): Either[EE,AA] = this match {
+    case Left(_) => b
+    case Right(a) => Right(a)
+  }
 }
 
 case class Left[+E](value: E) extends Either[E, Nothing]
