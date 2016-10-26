cbd375b273cc4f4df3717f6a34dad74984565bed
hui <zhanghuizaizhe@hotmail.com>
2016-10-26 23:17:48 +0800

    implement the function Map() and flatMap() of Either

---
+++ src/main/scala/fp/datastructs/Either.scala
@@ -0,0 +1,20 @@
+package fp.datastructs
+
+
+sealed trait Either[+E, +A] {
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Left(e) => Left(e)
+    case Right(a) => Right(f(a))
+  }
+
+  def flatMap[EE >: E, B](f: A => Either[EE,B]):Either[EE,B] = this match {
+    case Left(e) => Left(e)
+    case Right(a) => f(a)
+  }
+
+}
+
+case class Left[+E](value: E) extends Either[E, Nothing]
+
+case class Right[+A](value: A) extends Either[Nothing, A]
+
